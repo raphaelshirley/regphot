5fb4aaaaf308da6c41b9e33697d01f4b3fd3deff
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2016-12-15 16:48:54 +0000

    trying to structure code properly

---
+++ regphot/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 16:41:07 2016
+
+@author: rs548
+"""
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 15 16:40:10 2016
+
+@author: rs548
+"""
+
+#Don't really know how to use a setup.py file yet

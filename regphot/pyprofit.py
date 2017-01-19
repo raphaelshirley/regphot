0e34b4ad7fb0c1cf643483be4dd7c04e82e7bfc5
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2017-01-19 14:21:03 +0000

    started a couple of new files and starting to build galfit wrapper

---
+++ regphot/pyprofit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 14:18:44 2017
+
+This code should be analogous to galfit.py and provide all the same functions
+such that the two can be interchanged freely in order to run tests and
+comparisons
+
+@author: rs548
+"""
+

d2dd848e7d6abaf35903b71b1d705010d7dbfab5
Raphael Shirley <Raphael.Shirley@sussex.ac.uk>
2017-03-17 18:08:27 +0000

    End of week. Started Simard ipynb for investigating parameter space and creating prior func.

--- regphot/mcmc.py
+++ regphot/mcmc.py
@@ -30,87 +30,30 @@ the data twice'.
 #import emcee
 #import galfit
 import numpy as np
+from scipy.stats import multivariate_normal
+import pymultinest
+import math, os
+if not os.path.exists("chains"): os.mkdir("chains")
 
-testModel = [['sersic',
-              1791., 
-              1222.,
-              19.,
-              107.,
-              0.8,
-              0.0000,
-              0.0000,
-              0.0000,
-              0.7,
-              -8.5],
-             ['sersic',
-              1902., 
-              1387.,
-              22.,
-              37.,
-              0.,
-              0.0000,
-              0.0000,
-              0.0000,
-              0.,
-              -53.],
-             ['sky',
-              -1.651e-03,
-              0.000e+00,
-              0.000e+00]]
-
-def fakeTestChi2(image,model):
-    print('fake test chisquared fucntion doesnt use input image ',image)
-    
-    trueModel =  [['sersic',
-              1790.7958, 
-              1222.2855,
-              19.4770,
-              107.7908,
-              0.8121,
-              0.0000,
-              0.0000,
-              0.0000,
-              0.7140,
-              -8.5677],
-             ['sersic',
-              1902.9004,
-              1387.3171,
-              22.2516,
-              37.0798,
-              0.7359,
-              0.0000,
-              0.0000,
-              0.0000,
-              0.4275,
-              -53.2329],
-             ['sky',
-              -1.651e-03,
-              0.000e+00,
-              0.000e+00]]
-    chiSquared = 0
-    for componentN in range(1,len(model)):
-        component = model[componentN]
-        testcomponent = trueModel[componentN]
-        for m in range(1,len(component)):
-            chiSquared = chiSquared + (component[m] - testcomponent[m])**2.0
-        
-    #the following gives the ln of the gaussian
-    return chiSquared
-
-def fakeTestLogChi2(image,model):
-    return np.log(fakeTestChi2(image,model))
-
-d = fakeTestLogChi2('/path/to/fakeimage.fits', testModel)
-
-
-
-def lnprior(w):
+def prior(mean,cov):
     """
     Returns the prior probabilty over the parameter vector w. This can be built 
     on a multivariate Gaussian over the parameter space for a given number of 
     Sersic profiles
+    
+    inputs:
+        w,      1-d array,      parameter vector
+        cov,    array,          covariance matrix for parameter
+        
+    outputs:
+        prior,  scipy object,   normalised probability density for gaussian 
+                                prior with specified covariance matrix the
+                                output object can generate a pdf for a given 
+                                parameter vector by calling "prior.pdf(w)"
     """
-    numSersics = len(w)
+    prior  = multivariate_normal(mean,cov)
+    
+    return prior
     
     
     
@@ -124,13 +67,17 @@ def lnlike(image,w):
     
     """
     chi2 = chi2('pyprofit',w)
+    return lnlike
 
-def multinestevidence():
+def multinestevidence(mean,cov,model):
     """
     Input prior over paramters and black box function for evaluating ln liklihood
     based on chi2 from image/model and calcualte evidence for given model
     in preferebly the exact same way to emcee to allow comparison
     """
+    prior1 = prior(mean,cov)
+    pymultinest.run(lnlike, prior1.pdf, n_params, 
+                    resume = True, verbose = True)
     
 def emceeevidence():
     """
--- setup.py
+++ setup.py
@@ -3,10 +3,28 @@
 """
 Created on Thu Dec 15 16:40:10 2016
 
+REGPHOT: Resolved Galactic PHOTometry
+
+This code is designed for investigating models of galaxy profiles in a very 
+general/robust way for images of highly blended resolved galaxies.
+
+It will calculate the Bayesian evidence for a model with a given number of 
+Sersic/Gaussian profiles in order to determine how many to use and will
+then provide a maximum liklihood (chi squared minimum) output parameters
+for the favoured model.
+
+Dr Raphael Shirley
+Daphne Jackson Research Fellow
+Astronomy Centre
+University of Sussex
+www.raphaelshirley.co.uk
+@raphaelshirley
+raphael.shirley@sussex.ac.uk
+
 @author: rs548
 """
 
-#Don't really know how to use a setup.py file yet
+
 
 from setuptools import setup
 
